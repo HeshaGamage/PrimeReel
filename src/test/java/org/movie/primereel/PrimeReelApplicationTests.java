7a3349c04bd90002b826c28f2e4e7b4fad662f42
Heshan Gamage <heshank92@gmail.com>
2025-05-17 20:48:06 +0530

    Initial Commit

---
+++ src/test/java/org/movie/primereel/PrimeReelApplicationTests.java
@@ -0,0 +1,13 @@
+package org.movie.primereel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeReelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
